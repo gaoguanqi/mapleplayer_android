4447936cc8fc99a7f96926e7b205f2a5475bee6e
gaoguanqi <307590625@qq.com>
2019-12-08 20:44:46 +0800

    welcome

--- app/src/main/java/com/maple/player/base/BaseViewModel.kt
+++ app/src/main/java/com/maple/player/base/BaseViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 import com.maple.player.app.MyApplication
 
 open class BaseViewModel(app:MyApplication) : AndroidViewModel(app) {
-
     private lateinit var app:MyApplication
+
     init {
         this.app = app
     }

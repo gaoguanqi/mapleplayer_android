d2566a53f9e34a10b0f8a9276f212a1112f07dee
gaoguanqi <307590625@qq.com>
2020-06-27 11:11:03 +0800

    BASE_URL

--- app/src/main/java/com/maple/player/app/config/Config.kt
+++ app/src/main/java/com/maple/player/app/config/Config.kt
@@ -3,7 +3,8 @@ package com.maple.player.app.config
 class Config {
     companion object{
         //uid 468787165
-        const val BASE_URL:String = "http://47.94.147.138:3000"
+//        const val BASE_URL:String = "http://47.94.147.138:3000"
+        const val BASE_URL:String = "http://imaple.vip:3000"
 
         const val SUCCESS_CODE:Int = 200
 
